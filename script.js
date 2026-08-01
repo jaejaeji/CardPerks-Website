260d4799d39fcde351bbad179b07e5cf8296120f
jaejaeji <wogurji@gmail.com>
2026-07-31 21:19:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function showMenu() {
+    var navbar = document.getElementById("navbar");
+    if (navbar.style.display === "none") {
+        navbar.style.display = "block";
+    } else {
+        navbar.style.display = "none";
+    }
+}
+
+function showBenefits() {
+    var benefits = document.getElementById("benefits_saph_pref");
+    if (benefits.style.display === "none") {
+        benefits.style.display = "block";
+    } else {
+        benefits.style.display = "none";
+    }
+}
+
+function showBenefits2() {
+    var benefits2 = document.getElementById("benefits_free_un");
+    if (benefits2.style.display === "none") {
+        benefits2.style.display = "block";
+    } else {
+        benefits2.style.display = "none";
+    }
+}
+
