2417c83da53fd35be6850044da77c27556f3ed46
jaejaeji <wogurji@gmail.com>
2026-08-11 01:31:38 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -18,3 +18,47 @@ function showBenefits(cardNumber) {
   });
   document.getElementById("card" + cardNumber).style.display = "flex"; // Show the selected card
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+// let mouseDown = false;
+// let startX, scrollLeft;
+// const slider = document.querySelector('.parent');
+
+// const startDragging = (e) => {
+//   mouseDown = true;
+//   startX = e.pageX - slider.offsetLeft;
+//   scrollLeft = slider.scrollLeft;
+// }
+
+// const stopDragging = (e) => {
+//   mouseDown = false;
+// }
+
+// const move = (e) => {
+//   e.preventDefault();
+//   if(!mouseDown) { return; }
+//   const x = e.pageX - slider.offsetLeft;
+//   const scroll = x - startX;
+//   slider.scrollLeft = scrollLeft - scroll;
+// }
+
+// // Add the event listeners
+// slider.addEventListener('mousemove', move, false);
+// slider.addEventListener('mousedown', startDragging, false);
+// slider.addEventListener('mouseup', stopDragging, false);
+// slider.addEventListener('mouseleave', stopDragging, false);
