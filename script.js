1c532045630429fb6dd3dfe4c20d6eaf10b4589f
jaejaeji <wogurji@gmail.com>
2026-08-10 21:30:11 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-function showMenu() {
-    var navbar = document.getElementById("navbar");
-    if (navbar.style.display === "none") {
-        navbar.style.display = "block";
-    } else {
-        navbar.style.display = "none";
-    }
+
+// Menu bar functions
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
 }
 
-function showBenefits() {
-    var benefits = document.getElementById("benefits_saph_pref");
-    if (benefits.style.display === "none") {
-        benefits.style.display = "block";
-    } else {
-        benefits.style.display = "none";
-    }
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
 
-function showBenefits2() {
-    var benefits2 = document.getElementById("benefits_free_un");
-    if (benefits2.style.display === "none") {
-        benefits2.style.display = "block";
-    } else {
-        benefits2.style.display = "none";
-    }
-}
 
+// myCards page - tabbed interface
+function showBenefits(cardNumber) {
+  const cards = document.querySelectorAll(".benefits");
+
+  cards.forEach(card => {
+      card.style.display = "none"; // Hide all cards
+  });
+  document.getElementById("card" + cardNumber).style.display = "flex"; // Show the selected card
+}
